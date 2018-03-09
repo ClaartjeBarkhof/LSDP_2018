4d5ea155c17010c3f24003e737d85b14d0a40c01
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2018-03-09 12:49:46 +0100

    arie

--- program-y/src/programy/dynamic/variables/train.py
+++ program-y/src/programy/dynamic/variables/train.py
@@ -19,6 +19,11 @@ from programy.dynamic.variables.variable import DynamicVariable
 
 from programy.utils.text.dateformat import DateFormatter
 
+import requests
+import pprint
+from xml.etree import ElementTree
+from datetime import datetime
+from login import aut
 
 class GetTrain(DynamicVariable):
 
@@ -26,7 +31,23 @@ class GetTrain(DynamicVariable):
         DynamicVariable.__init__(self, config)
 
     def get_value(self, bot, clientid, value=None):
-        formatter = DateFormatter()
-        print("HELLO")
-        print(type(formatter.time_representation()))
-        return formatter.time_representation()
+        print("TEST",value)
+        global aut
+        if value != None:
+        	return self.first_leaving_train(value, aut)
+
+    def first_leaving_train(name, aut):
+	    ns = 'https://webservices.ns.nl/'
+	    r = requests.get(ns + 'ns-api-avt?station=' + name, auth=aut)
+	#     print(r.text)
+	    tree = ElementTree.fromstring(r.text)
+	    train = tree.find('VertrekkendeTrein')
+	    treinsoort = train.find('TreinSoort').text
+	    eindbest = train.find('EindBestemming').text
+	    spoor = train.find('VertrekSpoor').text
+	    vertrektijd = train.find('VertrekTijd').text
+	    vertrektijd = datetime.strptime(vertrektijd, '%Y-%m-%dT%H:%M:%S%z')
+	    return 'The next leaving train is the ' + treinsoort + \
+	          ' to ' + eindbest + \
+	          ' at ' + vertrektijd.time().isoformat(timespec='minutes') + \
+	          ' from platform ' +  spoor

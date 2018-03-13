33fe41771a9b019f26f68fd52ded65cddf867818
ariesoeteman <awsoeteman@hotmail.com>
2018-03-13 17:50:09 +0100

    last train en meer

--- program-y/src/programy/dynamic/maps/train.py
+++ program-y/src/programy/dynamic/maps/train.py
@@ -23,6 +23,18 @@ from xml.etree import ElementTree
 from datetime import datetime
 from login import aut
 
+def time_to_string(time):
+    h = time.time().hour
+    m = time.time().minute
+    return('%s'%h + ':%s'%m)
+
+def find_last_today(opties):
+    for index, optie in enumerate(opties):
+        date1 = optie.find('GeplandeVertrekTijd').text[0:10]
+        date2 = opties[index+1].find('GeplandeVertrekTijd').text[0:10]
+        if date1 != date2:
+            return optie
+
 class GetTrain(DynamicMap):
 
     def __init__(self, config):
@@ -44,8 +56,53 @@ class GetTrain(DynamicMap):
         spoor = train.find('VertrekSpoor').text
         vertrektijd = train.find('VertrekTijd').text
         vertrektijd = datetime.strptime(vertrektijd, '%Y-%m-%dT%H:%M:%S%z')
-        
+
+        vertrektijd = time_to_string(vertrektijd)
  #       print(vertrektijd.time().hour + vertrektijd.time().minute)
-        h = vertrektijd.time().hour
-        m = vertrektijd.time().minute
-        return ('the next leaving train is the ' + treinsoort +' to ' + eindbest +' at ' + '%s'%h + ':%s'%m + ' from platform ' +  spoor)
+        return ('the next leaving train is the ' + treinsoort +' to ' + eindbest +' at ' + vertrektijd + ' from platform ' +  spoor)
+
+
+#Dit doet het als de tijd 'LAST' is. Moet nog gebeuren voor echte tijdswaarden.
+class GetTrainTriple(DynamicMap):
+    def __init__(self, config):
+        DynamicMap.__init__(self, config)
+
+    def map_value(self, bot, clientid, input_value):
+        return self.triple_to_train(input_value)
+
+    def triple_to_train(self, name):
+        origin, time, destination = name.split(' , ')
+        if (time == 'LAST'):
+            time = '2018-03-13T23:50'
+        ns = 'https://webservices.ns.nl/'
+        global aut
+        r = requests.get(ns + 'ns-api-treinplanner?toStation=' + destination + '&fromStation=' + origin + '&dateTime=' + time, auth=aut)
+        tree = ElementTree.fromstring(r.text)
+        opties = tree.findall('ReisMogelijkheid')
+        optie = find_last_today(opties)
+
+        overstappen = optie.find('AantalOverstappen').text
+        reistijd = optie.find('GeplandeReisTijd').text
+        vertrektijd = optie.find('GeplandeVertrekTijd').text
+        actuelevertrektijd = optie.find('ActueleVertrekTijd').text
+        aankomsttijd = optie.find('GeplandeAankomstTijd').text
+        actueleaankomsttijd = optie.find('ActueleAankomstTijd').text
+        vertrektijd = datetime.strptime(vertrektijd, '%Y-%m-%dT%H:%M:%S%z')
+        vertrektijd = time_to_string(vertrektijd)
+
+        output='tijd: '+vertrektijd+' .'
+        counter = 0
+        for reisdeel in optie.findall('ReisDeel'):
+            spoor = reisdeel.find('ReisStop').find('Spoor').text
+            # print("SPOORTJE")
+            # print(spoor)
+            if counter == 0:
+                # print('vertrek van ' + origin + ' op spoor: ' + spoor)
+                output += 'vertrek van ' + origin + ' op spoor: ' + spoor
+            if counter > 0:
+                overstap = reisdeel.find('ReisStop').find('Naam').text
+                # print('overstappen op ' + overstap + ' op spoor: ' + spoor)
+                output += ', overstappen op ' + overstap + ' op spoor: ' + spoor
+            counter += 1
+        output += '.'
+        return output
